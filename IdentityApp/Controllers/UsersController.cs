d6074fb778b7942819b26bb6e50cc8b64a8590ee
canersinan00 <canersinan1013@gmail.com>
2025-10-13 15:25:29 +0300

    User Update Page and Edit Functions

--- IdentityApp/Controllers/UsersController.cs
+++ IdentityApp/Controllers/UsersController.cs
@@ -22,7 +22,7 @@ namespace IdentityApp.Controllers
             return View();
         }
         [HttpPost]
-        public async Task <IActionResult> Create(CreateViewModel model)
+        public async Task<IActionResult> Create(CreateViewModel model)
         {
             if (ModelState.IsValid)
             {
@@ -38,9 +38,66 @@ namespace IdentityApp.Controllers
                 {
                     return RedirectToAction("Index");
                 }
-                foreach(IdentityError error in result.Errors)
+                foreach (IdentityError error in result.Errors)
                 {
                     ModelState.AddModelError("", error.Description);
+                }
+            }
+            return View(model);
+        }
+
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            var user = await _userManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                return View(new EditViewModel
+                {
+                    Id = user.Id,
+                    FullName = user.FullName,
+                    Email = user.Email
+                });
+
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(string id, EditViewModel model)
+        {
+            if (id != model.Id)
+            {
+                return RedirectToAction("Index");
+            }
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByIdAsync(model.Id);
+                if (user != null)
+                {
+                    user.FullName = model.FullName;
+                    user.Email = model.Email;
+
+                    var result = await _userManager.UpdateAsync(user);
+
+                    if (result.Succeeded && !string.IsNullOrEmpty(model.Password))
+                    {
+                        await _userManager.RemovePasswordAsync(user);
+                        await _userManager.AddPasswordAsync(user, model.Password);
+                    }
+
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                    foreach(IdentityError error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
                 }
             }
             return View(model);
---
+++ IdentityApp/ViewModels/EditViewModel.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace IdentityApp.ViewModels
+{
+    public class EditViewModel
+    {
+        public string? Id { get; set; }
+        
+        public string? FullName { get; set; }
+        
+        [EmailAddress]
+        public string? Email { get; set; }
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Password and Confirm Password do not match.")]
+        public string? ConfirmPassword { get; set; }
+    }
+}
