ac1ed8c6808da70534f70a1e206d312b5094f3a0
canersinan00 <canersinan1013@gmail.com>
2025-10-16 16:14:28 +0300

    Account and Login

--- IdentityApp/ViewModels/LoginViewModel.cs
+++ IdentityApp/ViewModels/LoginViewModel.cs
@@ -5,9 +5,9 @@ namespace IdentityApp.ViewModels
     public class LoginViewModel
     {
         [EmailAddress]
-        public string? Email { get; set; } = null!;
+        public string Email { get; set; } = null!;
         [DataType(DataType.Password)]
-        public string? Password { get; set; } = null!;
+        public string Password { get; set; } = null!;
         public bool RememberMe { get; set; } = true;
     }
 }
